29265cee8adeaaf7a904edb2ac3f8f6c9524a4e7
Vicki <458044962@qq.com>
2020-08-27 21:03:36 +0800

    [refactor-homework][vicki]test: add 'rank case 1. Voyage is west-indies and length is 10.' test

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,10 +1,33 @@
+import test from "ava";
+import {rating} from "../src/rank";
+
 const rankTest = require('ava');
 
-rankTest('foo', t => {
-  t.pass();
+
+test('rank case 1. Voyage is west-indies and length is 10. ', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+
+  const result = rating(voyage,history);
+
+  t.is(result, 'B');
 });
 
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
-});
