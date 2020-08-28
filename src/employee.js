d950d633314cd4d083c6be3525e4ca31d7c2a2be
Vicki <458044962@qq.com>
2020-08-29 01:07:47 +0800

    [refactor-homework][vicki]Refactor: format code

--- src/employee.js
+++ src/employee.js
@@ -1,36 +1,33 @@
 const ENGINEER = 'engineer';
-
 const MANAGER = 'manager';
-
 const SALESMAN = 'salesman';
 
 class Employee {
-  constructor (name, type) {
-    this.validateType(type);
-    this._name = name;
-    this._type = type;
-  }
-
-  validateType (type) {
-    if (!this.judgeType(type)) {
-      throw new Error(`Employee cannot be of type ${type}`);
+    constructor(name, type) {
+        this.validateType(type);
+        this._name = name;
+        this._type = type;
     }
-  }
 
-  judgeType(type) {
-    return [
-      ENGINEER,
-      MANAGER,
-      SALESMAN,
-    ].includes(type);
-  }
+    validateType(type) {
+        if (!this.judgeType(type)) {
+            throw new Error(`Employee cannot be of type ${type}`);
+        }
+    }
 
-  toString () {
-    return `${this._name} (${this._type})`;
-  }
+    judgeType(type) {
+        return [
+            ENGINEER,
+            MANAGER,
+            SALESMAN,
+        ].includes(type);
+    }
+
+    toString() {
+        return `${this._name} (${this._type})`;
+    }
 }
 
-
 module.exports = {
-  Employee
+    Employee
 };
