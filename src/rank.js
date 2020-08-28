1d20becab35de06f2d6291223b4a67176b49c526
Vicki <458044962@qq.com>
2020-08-28 11:43:17 +0800

    [refactor-homework][vicki]Refactor: Merge voyage.length > 18

--- src/rank.js
+++ src/rank.js
@@ -44,11 +44,8 @@ function voyageProfitFactor (voyage, history) {
     if (history.length > 10) {
       result += 1;
     }
-    if (voyage.length > 12) {
+    if (voyage.length > 12 && voyage.length <19) {
       result += 1;
-    }
-    if (voyage.length > 18) {
-      result -= 1;
     }
   }
   else {
