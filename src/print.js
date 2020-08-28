6bcc03ee8664b01be38a2b579e2ac72286d386d1
Vicki <458044962@qq.com>
2020-08-28 19:26:42 +0800

    [refactor-homework][vicki]Refactor: delete note

--- src/print.js
+++ src/print.js
@@ -4,16 +4,13 @@ function printOwing (invoice) {
   console.log('**** Customer Owes ****');
   console.log('***********************');
 
-  // calculate outstanding
   for (const o of invoice.borderSpacing) {
     outstanding += o.amount;
   }
 
-  // record due date
   const today = new Date();
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  // print details
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
